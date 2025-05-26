9cfb5aa95de15c1be102c42ca411f38e524688f5
H.V. Sarvesh Vishwanath <135261804+H-V-Sarvesh@users.noreply.github.com>
2025-05-26 21:10:52 +0530

    Array_Average.java

--- Array_Average.java
+++ Array_Average.java
@@ -7,15 +7,6 @@ public class Array_Average {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		/*
-		 * Scanner sc = new Scanner(System.in);
-		 * System.out.println("enter the array elements: "); int sum = 0; int[] arr =
-		 * new int[5]; for (int i = 0; i < arr.length; i++) { arr[i] = sc.nextInt();
-		 * //sum = arr[i] + arr[i++]; } for (int i = 0; i < arr.length; i++) { while
-		 * (arr[i] <= arr[5]) { sum = arr[i]; arr[i]++; } // sum = arr[i] + arr[i+1];
-		 * break; } int avg = sum / 5; System.out.println(avg); sc.close();
-		 */
-
 		Scanner scanner = new Scanner(System.in);
 		int numbers[] = new int[100];
 		int sum = 0;
